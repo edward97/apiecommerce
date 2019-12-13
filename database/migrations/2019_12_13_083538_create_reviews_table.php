28a48dba922bafbe153efaef0407d30805db388b
edward97 <edw.suryajaya@gmail.com>
2019-12-13 15:50:54 +0700

    Migration created for product & reviews

--- database/migrations/2019_12_13_083538_create_reviews_table.php
+++ database/migrations/2019_12_13_083538_create_reviews_table.php
@@ -15,7 +15,15 @@ class CreateReviewsTable extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('customer');
+            $table->text('review');
+            $table->integer('star');
             $table->timestamps();
+
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')
+                ->references('id')->on('products')
+                ->onDelete('cascade');
         });
     }
 
